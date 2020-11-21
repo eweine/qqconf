e4b14fa696e2bf8f034d88aed8204ff36a813ce8
Eric <ericweine15@gmail.com>
2020-11-21 12:06:30 -0600

    last commit of one sided code before deletion

--- qqconf/R/one_sided.R
+++ qqconf/R/one_sided.R
@@ -78,21 +78,52 @@ get_level_from_bounds_one_sided <- function(bounds,
 
 }
 
-# Not sure what error options to give the user here
-get_bounds_onesided <- function(alpha, n, tol = 1e-8, max_it = 50) {
+#' Calculates Local Bounds For One-Sided Test
+#'
+#' Given the size of the dataset and the desired Type I Error rate \eqn{\alpha},
+#' this calculates one-sided testing bounds for each order statistic.
+#' These bounds are created using "equal local levels," which means that
+#' each bound corresponds to a test that has a Type I Error rate of \eqn{\eta}
+#' on its corresponding order statistic. Note that this code may be very slow for
+#' values of n.
+#'
+#' @param alpha Desired Type I Error rate of the complete test.
+#' @param n Size of the dataset.
+#' @param tol (Optional) Relative tolerance of the \eqn{\alpha} level of the
+#' simultaneous test. Defaults to 1e-8.
+#' @param max_it (Optional) Maximum number of iterations of Binary Search Algorithm
+#' used to find the bounds. Defaults to 100 which should be much larger than necessary
+#' for a reasonable tolerance.
+#'
+#' @return A list with components
+#' \itemize{
+#'   \item bound - Numeric vector containing the bounds of the test of each order statistic.
+#'   \item x - Expectation of each order statistic. This is the x-axis for the bounds if used in a qq-plot.
+#'   \item local_level - Type I Error rate of each individual test on the order statistic. It is equal for all tests.
+#' }
+#'
+#' @examples
+#' get_level_from_bounds_onesided(alpha = .05, n = 100, tol = 1e-6, max_it = 50)
+#'
+#'
+#' @export
+get_bounds_onesided <- function(alpha, n, tol = 1e-8, max_it = 100) {
 
-  eta_high <- asymp_eta(alpha, n)
+  eta_high <- -log(1 - alpha) / (2 * log(log(n)) * log(n))
   eta_low <- alpha / n
   eta_curr <- eta_low + (eta_high - eta_low) / 2
   n_it <- 0
+  rel_err_tol <- (1 / alpha) * tol * (10 ^ (-4))
+  bin_search_tol <- tol - rel_err_tol
 
   while (n_it < max_it) {
 
     n_it <- n_it + 1
     h_vals <- qbeta(eta_curr, 1:n, n:1)
-    test_alpha <- 1 - get_level_from_bounds_one_sided(h_vals)
 
-    if (abs(test_alpha - alpha) / alpha <= tol) break
+    test_alpha <- 1 - get_level_from_bounds_one_sided(h_vals, approx = FALSE)
+
+    if (abs(test_alpha - alpha) / alpha <= bin_search_tol) break
 
     if (test_alpha > alpha) {
 
@@ -112,6 +143,6 @@ get_bounds_onesided <- function(alpha, n, tol = 1e-8, max_it = 50) {
   beta_vec <- n - alpha_vec + 1
   order_stats_mean <- alpha_vec / (alpha_vec + beta_vec)
 
-  return(list(h = h_vals, x = order_stats_mean, local_level = eta_curr))
+  return(list(bound = h_vals, x = order_stats_mean, local_level = eta_curr))
 
 }
